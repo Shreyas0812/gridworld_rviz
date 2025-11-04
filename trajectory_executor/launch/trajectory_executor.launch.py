43b2876e2adf094286fe8cacdfe8fcd956537848
Shreyas Raorane <raorane@seas.upenn.edu>
2025-11-04 18:15:03 -0500

    launch file and cpp file tied together, reading the correct csv file

--- trajectory_executor/launch/trajectory_executor.launch.py
+++ trajectory_executor/launch/trajectory_executor.launch.py
@@ -1,27 +1,38 @@
 from launch import LaunchDescription
-from launch.substitutions import LaunchConfiguration
-from launch.actions import DeclareLaunchArgument
+from launch.substitutions import PathJoinSubstitution, LaunchConfiguration
+from launch.actions import DeclareLaunchArgument, LogInfo
 
+from launch_ros.substitutions import FindPackageShare
 from launch_ros.actions import Node
 
 def generate_launch_description():
-    use_sim_time = LaunchConfiguration('use_sim_time', default='false')
 
-    use_sim_time_arg = DeclareLaunchArgument(
-        'use_sim_time',
-        default_value='false',
-        description='Use simulation (Gazebo) clock if true'
+    data_file_arg = DeclareLaunchArgument(
+        'data_file',
+        default_value='test_generated_trajectories',
+        description='Name of the trajectory data CSV file'
     )
 
-    trajectory_executor_node = Node(
-        package='trajectory_executor',
-        executable='trajectory_executor',
-        name='trajectory_executor',
-        output='screen',
-        parameters=[{'use_sim_time': use_sim_time}]
-    )
+    trajectory_executor_params = PathJoinSubstitution([
+        FindPackageShare("trajectory_executor"),
+        "data",
+        [LaunchConfiguration("data_file"), ".csv"]
+    ])
 
     return LaunchDescription([
-        use_sim_time_arg,
-        trajectory_executor_node,
+        
+        data_file_arg,
+
+        LogInfo(msg=['Using trajectory data file: ', trajectory_executor_params]),
+
+        Node(
+            package='trajectory_executor',
+            executable='trajectory_executor',
+            name='trajectory_executor',
+            output='screen',
+            parameters=[{
+                'data_file': trajectory_executor_params
+            }]
+            # arguments=['--ros-args', '--log-level', 'debug']
+        ),
     ])
--- trajectory_executor/src/trajectory_executor.cpp
+++ trajectory_executor/src/trajectory_executor.cpp
@@ -1,4 +1,17 @@
 #include "rclcpp/rclcpp.hpp"
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <string>
+
+struct AgentPosition {
+  int agent_id;
+  int timestep;
+  int location;
+  double x;
+  double y;
+  int orientation;
+};
 
 class TrajectoryExecutor : public rclcpp::Node
 {
@@ -6,6 +19,20 @@ public:
     TrajectoryExecutor() : Node("trajectory_executor")
     {
         RCLCPP_INFO(this->get_logger(), "trajectory executor running");
+        // Declare parameters
+        this->declare_parameter<std::string>("data_file", "trajectories.csv");
+
+        // Get parameters
+        std::string data_file_ = this->get_parameter("data_file").as_string();
+        RCLCPP_INFO(get_logger(), "Loading trajectory data from: %s", data_file_.c_str());
+    }
+private:
+    bool loadCSV(const std::string & filename)
+    {
+        RCLCPP_INFO(this->get_logger(), "Loading CSV file: %s", filename.c_str());
+        std::ifstream file(filename);
+        
+        return true;
     }
 };
 
